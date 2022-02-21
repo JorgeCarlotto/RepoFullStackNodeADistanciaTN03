1cc40752afb82650ade72a049f75d842a04536c3
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-02-20 23:52:28 -0300

    Clase 5

---
+++ Clase_5_JSON,_mas_condicionales_y_ciclos/App de Notas/app.js
@@ -0,0 +1,26 @@
+/* APLICACION DE TAREAS */
+
+
+let funcionesDeTareas =  require('./funcionesDeTareas');
+let accion = process.argv[2];
+
+switch (accion) {
+    case 'listar':
+        funcionesDeTareas.todasLasTareas();
+        break;
+        
+    case 'incompletas':
+        funcionesDeTareas.tareasIncompletas();
+        break;
+    
+    case undefined:
+        console.log('Atención - Tienes que pasar una Acción');
+        break;
+
+    default:
+        console.log('No entiendo qué quieres hacer');
+        break;
+     
+}
+
+console.log(accion)
---
+++ Clase_5_JSON,_mas_condicionales_y_ciclos/App de Notas/funcionesDeTareas.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+let funcionesDeTareas = {
+
+    archivo: 'tareas.json',
+
+    leerJSON: function() {
+        let stringTareas = fs.readFileSync(this.archivo ,'utf-8');
+        return JSON.parse(stringTareas);
+    },
+
+
+    todasLasTareas: function(){
+
+        let tareas = this.leerJSON();
+
+        console.log();    
+        console.log('Listado de tareas');
+        console.log('----------------');
+
+        for (let i=0; i < tareas.length ; i++){
+            console.log(i+1 +'. ' + tareas[i].titulo + ' - ' + tareas[i].estado);
+        }
+
+    },
+
+    tareasIncompletas: function() {
+
+        let tareas = this.leerJSON();
+
+        console.log();
+        console.log('Mostrado tareas incompletas')
+        console.log('---------------------------');
+
+        for (let i=0; i < tareas.length ; i++){
+
+            if(tareas[i].estado != 'completado' ){
+                console.log(i+1 +'. ' + tareas[i].titulo + ' -> ' + tareas[i].estado);
+            }
+        }
+    },
+
+
+};
+
+module.exports = funcionesDeTareas;
